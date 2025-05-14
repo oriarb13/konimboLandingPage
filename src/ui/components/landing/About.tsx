67880f8c5dc73c5716518b9ee92cd1717bd2bb65
oriarb13 <oriarb13690@gmail.com>
2025-05-14 12:46:46 +0300

    fix text responsive

--- src/ui/components/landing/About.tsx
+++ src/ui/components/landing/About.tsx
@@ -29,18 +29,18 @@ const About = () => {
           />
         </div>
 
-        <div className="w-full md:w-1/2 px-4 md:px-6 lg:px-8 py-6 md:py-4 lg:py-6 flex flex-col justify-center bg-zinc-50 text-zinc-700 dark:bg-zinc-700 dark:text-zinc-50 ">
-          <h1 className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-bold mb-3 md:mb-4 lg:mb-6">
+        <div className="w-full md:w-1/2 px-4 md:px-6 lg:px-4 xl:px-6 2xl:px-8 py-6 md:py-4 lg:py-1 xl:py-3 2xl:py-6 flex flex-col justify-center bg-zinc-50 text-zinc-700 dark:bg-zinc-700 dark:text-zinc-50 ">
+          <h1 className="text-2xl sm:text-3xl md:text-3xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-bold mb-3 md:mb-4 lg:mb-1 xl:mb-3 2xl:mb-6">
             {t("landing.about.title")}
           </h1>
-          <div className="space-y-2 md:space-y-3 lg:space-y-4">
-            <p className="text-base sm:text-lg md:text-lg lg:text-xl">
+          <div className="space-y-2 md:space-y-3 lg:space-y-1 xl:space-y-2 2xl:space-y-4">
+            <p className="text-base sm:text-lg md:text-lg lg:text-md xl:text-lg 2xl:text-xl">
               {t("landing.about.p1")}
             </p>
-            <p className="text-base sm:text-lg md:text-lg lg:text-xl">
+            <p className="text-base sm:text-lg md:text-lg lg:text-md xl:text-lg 2xl:text-xl">
               {t("landing.about.p2")}
             </p>
-            <p className="text-base sm:text-lg md:text-lg lg:text-xl">
+            <p className="text-base sm:text-lg md:text-lg lg:text-md xl:text-lg 2xl:text-xl">
               {t("landing.about.p3")}
             </p>
           </div>
